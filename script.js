7174489a098176a6a087d81847416275883acf2e
William Diffey <william.diffey@protonmail.com>
2019-06-08 02:43:17 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function inputListener() {
+  $('#dog-form').on('submit', function(event) {
+    $('.dog-image-results').empty();
+    event.preventDefault();
+    let dogInput = $('#dog-input').val();
+    getInput(dogInput);
+  });
+}
+
+function getInput(dogInput) {
+    if (dogInput.length === 0) {
+      
+      let userInput = 3;
+      pullDogImages(userInput);
+
+    } else if (dogInput > 50) {
+      alert('Less than 50 dogs pictures please');
+    
+    } else if (dogInput < 1 ) {
+      alert('Please pick a positive number');
+    
+    } else {
+      
+      let userInput = dogInput;
+      pullDogImages(userInput);
+    }
+  
+  }
+
+function pullDogImages(userInput) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${userInput}`)
+    .then(response => response.json())
+
+    .then(responseJson => 
+ 
+         showDogs(responseJson))
+    .catch(error => alert('Something went wrong'));
+}
+
+function showDogs(finalPulledDogImages) {
+    for (var i = 0; i < finalPulledDogImages.message.length; i++) {
+      $('.dog-image-results').append(`<img src="${finalPulledDogImages.message[i]}">`);
+    };
+}
+
+$(function() {
+  inputListener();
+}
+);
